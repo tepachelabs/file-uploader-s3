635d1fbb0aba3fefb313cf2b0b8e93d5d59b9283
Ariel Plascencia <imariel2d@gmail.com>
2023-03-19 09:12:41 -0700

    initial commit

---
+++ components/file-uploader/index.js
@@ -0,0 +1,9 @@
+export const FileUploader = () => {
+
+  return (
+    <form>
+      <input type="file" />
+      <button type="submit">Submit File</button>
+    </form>
+  );
+};
---
+++ components/files-list/index.js
@@ -0,0 +1,9 @@
+export const FilesList = () => {
+
+  return (
+    <div>
+      <p>File 1...</p>
+      <p>File 2...</p>
+    </div>
+  );
+};
