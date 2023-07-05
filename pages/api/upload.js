ae80423a91f2327bd84e3d16c0e5824369613189
imariel2d <imariel2d@gmail.com>
2023-07-04 17:47:01 -0700

    New file uploader

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,27 +1,38 @@
+import { v4 as uuidv4 } from 'uuid';
 import { IncomingForm } from 'formidable';
+
 
 import { uploadFileToS3 } from '@/s3';
 
 export default function handler(req, res) {
   if (req.method !== 'POST') {
     res.status(400).json({ error: 'We only support POST method!' });
+    return;
   }
 
   const form = new IncomingForm();
 
-  form.parse(req, (err, fields, files) => {
-    uploadFileToS3(files.file)
-      .then(res => {
-        return res.status(201).send({ name: 'Your file was uploaded!' });
-      })
-      .catch((error) => {
-        return res.status(400).send({ error });
-      });
+  form.parse(req, async (err, fields, files) => {
+    try {
+      const { file } = files;
+      const { originalFilename } = file;
+
+      const fileExtension = originalFilename.substring(originalFilename.lastIndexOf('.'));
+      const newFileName = `${uuidv4()}${fileExtension}`;
+
+      const result = await uploadFileToS3(newFileName, file);
+      res.status(200).json(result);
+
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
   });
 }
 
 export const config = {
   api: {
     bodyParser: false,
+    externalResolver: true,
   },
 };
--- s3/index.js
+++ s3/index.js
@@ -1,28 +1,30 @@
+import { Upload } from '@aws-sdk/lib-storage';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+
 import fs from 'fs';
 
 const client = new S3Client({
-  region: process.env.AWS_BUCKET_REGION,
+  region: process.env.AWS_S3_BUCKET_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    accessKeyId: process.env.AWS_S3_PUBLIC_KEY,
+    secretAccessKey: process.env.AWS_S3_SECREY_KEY
   },
-
 });
 
 /**
  *
  * @return Promise<*>
  */
-export const uploadFileToS3 = (file) => {
-  const stream = fs.createReadStream(file.filepath);
+export const uploadFileToS3 = async (fileName, file) => {
+  const upload = new Upload({
+    client,
+    leavePartsOnError: false,
+    params: {
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: fileName,
+      Body: fs.createReadStream(file.filepath),
+    },
+  });
 
-  const uploadParams = {
-    Bucket: process.env.AWS_BUCKET_NAME,
-    Key: file.originalFilename,
-    Body: stream,
-  };
-
-  const command = new PutObjectCommand(uploadParams);
-  return client.send(command);
+  return upload.done();
 };
